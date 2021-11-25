2ab2e923e345e4d5b550b11b6760aa4b68b54f2c
MugiSus <42643211+MugiSus@users.noreply.github.com>
2021-11-25 15:48:57 +0900

    Update visual_text.js

--- visual_text.js
+++ visual_text.js
@@ -1,13 +1,9 @@
 let visualiseAsTxtRect =(x, y, w, h)=> {
     return (
         new Array(h).fill(0).map((_, i) =>  
-            new Array(w).fill(0).map((_, j) => {
-                switch (m[x + j]?.[y + i]) {
-                    case false: return '░░';
-                    case true: return '██';
-                    default: return '  ';
-                }
-            }).join("")
+            new Array(w).fill(0).map((_, j) => 
+                m[x + j]?.[y + i] == undefined ? '  ' : m[x + j][y + i] ? '██' : '░░'
+            ).join("")
         ).join("\n") + 
         ` [done ${new Date().getTime()}]`
     );
