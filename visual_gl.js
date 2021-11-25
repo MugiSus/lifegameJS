8faf1e3e912fd46c213f9dc8a44bf9578fd63afe
MugiSus <42643211+MugiSus@users.noreply.github.com>
2021-11-25 12:38:16 +0900

    Update visual_gl.js

--- visual_gl.js
+++ visual_gl.js
@@ -1,7 +1,11 @@
 const canvas = document.getElementById('canvas');
-[canvas.height, canvas.width] = [window.innerHeight, window.innerWidth];
-
 const gl = canvas.getContext('webgl');
+
+let resize =()=> {
+    [canvas.height, canvas.width] = [window.innerHeight, window.innerWidth];
+    gl.viewport(0, 0, canvas.width, canvas.height);
+}
+window.addEventListener('resize', resize);
 
 let viewportSize = 0;
 
@@ -43,12 +47,105 @@ const vertex = gl.getAttribLocation(program, "vertex");
 gl.enableVertexAttribArray(vertex);
 gl.vertexAttribPointer(vertex, 2, gl.FLOAT, false, 0, 0);
 
-let vertices = new Float32Array([
-    0.5, -0.5,
-    -0.5, -0.5,
-    -0.5, 0.5,
-    0.5, 0.5,
-]);
+// begin drawing
 
-gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.DYNAMIC_DRAW)
-gl.drawArrays(gl.LINE_LOOP, 0, vertices.length / 2);
+let scrollx = 0, scrolly = 0, zoom = 1;
+
+let getVertices =(scrollx, scrolly, zoom)=> {
+    let vertices = [];
+
+    Object.keys(m).forEach(x => 
+        Object.keys(m[x]).forEach(y => 
+            m[x][y] && vertices.push(
+                x * 100 - 45, y * 100 - 45,
+                x * 100 + 45, y * 100 - 45,
+                x * 100 + 45, y * 100 + 45,
+                x * 100 - 45, y * 100 - 45,
+                x * 100 + 45, y * 100 + 45,
+                x * 100 - 45, y * 100 + 45,
+            )
+        )
+    );
+
+    return Float32Array.from(vertices).map((value, index) => 
+        index % 2 ?
+        -(value - scrolly) * zoom / canvas.height :
+        (value - scrollx) * zoom / canvas.width
+    );
+}
+
+function main() {
+    let vertices = getVertices(scrollx, scrolly, zoom);
+
+    if (vertices.length) {
+        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.DYNAMIC_DRAW)
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 2);
+    }
+
+    requestAnimationFrame(main);
+}
+
+// galaxy
+
+cw(10, 10, 1);
+cw(11, 10, 1);
+cw(12, 10, 1);
+cw(13, 10, 1);
+cw(14, 10, 1);
+cw(15, 10, 1);
+cw(10, 11, 1);
+cw(11, 11, 1);
+cw(12, 11, 1);
+cw(13, 11, 1);
+cw(14, 11, 1);
+cw(15, 11, 1);
+
+cw(17, 10, 1);
+cw(17, 11, 1);
+cw(17, 12, 1);
+cw(17, 13, 1);
+cw(17, 14, 1);
+cw(17, 15, 1);
+cw(18, 10, 1);
+cw(18, 11, 1);
+cw(18, 12, 1);
+cw(18, 13, 1);
+cw(18, 14, 1);
+cw(18, 15, 1);
+
+cw(10, 13, 1);
+cw(10, 14, 1);
+cw(10, 15, 1);
+cw(10, 16, 1);
+cw(10, 17, 1);
+cw(10, 18, 1);
+cw(11, 13, 1);
+cw(11, 14, 1);
+cw(11, 15, 1);
+cw(11, 16, 1);
+cw(11, 17, 1);
+cw(11, 18, 1);
+
+cw(13, 17, 1);
+cw(14, 17, 1);
+cw(15, 17, 1);
+cw(16, 17, 1);
+cw(17, 17, 1);
+cw(18, 17, 1);
+cw(13, 18, 1);
+cw(14, 18, 1);
+cw(15, 18, 1);
+cw(16, 18, 1);
+cw(17, 18, 1);
+cw(18, 18, 1);
+
+scrollx = 1400;
+scrolly = 1400;
+
+setInterval(() => ep = e(ep), 100)
+
+// main
+
+resize();
+main();
