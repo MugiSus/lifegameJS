b6fd430dedcbd18170c153886484493cc3278a0e
MugiSus <42643211+MugiSus@users.noreply.github.com>
2021-12-06 14:06:22 +0900

    last touch to lifegame.js

--- lifegame.js
+++ lifegame.js
@@ -1,15 +1,15 @@
 let m = new Object(); // map
 let ep = new Array(); // positions to evaluate
 
-let w =(m, x, y, v)=> (m[x] = m[x] ?? new Object())[y] = v; // overwrite
+let w =(o, x, y, v)=> (o[x] = o[x] ?? new Object())[y] = v; // overwrite
 let r =(x, y)=> m[x]?.[y] ?? 0; // read
-let rv =(x, y)=> r(x - 1, y - 1) + r(x - 1, y) + r(x - 1, y + 1) + r(x, y - 1) + r(x, y + 1) + r(x + 1, y - 1) + r(x + 1, y) + r(x + 1, y + 1); // read vicinities
+let rv =(x, y)=> r(x - 1, y - 1) + r(x - 1, y) + r(x - 1, y + 1) + r(x, y - 1) + r(x, y + 1) + r(x + 1, y - 1) + r(x + 1, y) + r(x + 1, y + 1); // read 8 vicinities
 let gn =(x, y)=> r(x, y) ? rv(x, y) == 2 || rv(x, y) == 3 : rv(x, y) == 3; // get next
 
 let cw =(x, y, v)=> { // client write
     w(m, x, y, !!v); // write
     ep.push([x - 1, y - 1], [x - 1, y], [x - 1, y + 1], [x, y - 1], [x, y], [x, y + 1], [x + 1, y - 1], [x + 1, y], [x + 1, y + 1]); // add positions to evaluate
-};
+}
 
 let e =(ep)=> { // evaluate
     let nm = new Object(); // next map
