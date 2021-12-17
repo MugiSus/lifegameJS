0cf4c5b74ed9216ae096cd4a3316c3160f0b9392
MugiSus <42643211+MugiSus@users.noreply.github.com>
2021-12-17 14:12:23 +0900

    Update lifegame.js

--- lifegame.js
+++ lifegame.js
@@ -1,7 +1,7 @@
-let m = new Object(); // map
-let ep = new Array(); // positions to evaluate
+let m = {}; // new Object() // map
+let ep = []; // new Array() // positions to evaluate
 
-let w =(o, x, y, v)=> (o[x] = o[x] ?? new Object())[y] = v; // overwrite
+let w =(o, x, y, v)=> (o[x] = o[x] ?? {})[y] = v; // overwrite
 let r =(x, y)=> m[x]?.[y] ?? 0; // read
 let rv =(x, y)=> r(x - 1, y - 1) + r(x - 1, y) + r(x - 1, y + 1) + r(x, y - 1) + r(x, y + 1) + r(x + 1, y - 1) + r(x + 1, y) + r(x + 1, y + 1); // read 8 vicinities
 let gn =(x, y)=> r(x, y) ? rv(x, y) == 2 || rv(x, y) == 3 : rv(x, y) == 3; // get next
@@ -12,8 +12,8 @@ let cw =(x, y, v)=> { // client write
 }
 
 let e =(ep)=> { // evaluate
-    let nm = new Object(); // next map
-    let nep = new Array(); // next positions to evaluate
+    let nm = {}; // next map
+    let nep = []; // next positions to evaluate
     let x, y; // position
     ep.forEach(p => ( 
         [x, y] = p, // unpack
